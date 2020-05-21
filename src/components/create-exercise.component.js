52cdf30fc77d9fcb833dbf7b54bac6b839cecf11
Ben <ben@Macbooks-Air.standerline.lan>
2020-05-22 00:59:49 +0100

    complete

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -64,10 +64,10 @@ export default class CreateExercise extends Component {
         console.log(exercise);
 
         axios.post('http://localhost:5000/exercises/add', exercise)
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err));
-
-        window.location = '/';
+            .then(res => {
+                console.log(res.data);
+                window.location = '/';
+            });
     }
 
     onChangeDate(date) {
